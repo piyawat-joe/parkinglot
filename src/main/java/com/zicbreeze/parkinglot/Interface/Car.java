255108262623cfa4053b90bf5d53cf168b2547c1
Pitakpawatkul <piyawat.pitakpawatkul@lazada.com>
2018-08-06 19:23:33 +0700

    Setup Main class

---
+++ src/main/java/com/zicbreeze/parkinglot/Interface/Car.java
@@ -0,0 +1,8 @@
+package com.gojek.parkinglot.Interface;
+
+public interface Car {
+    public String color = "";
+    public String regisNo = "";
+    public void park();
+    public void leave();
+}
